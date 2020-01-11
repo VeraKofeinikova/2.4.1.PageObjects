7c0b2201633b79cdaaa214df5acb18416ed0667f
Vera Kofeinikova <verak-va@mail.ru>
2020-01-11 22:03:57 +0400

    один из тестов для функции логина пока вынесен в отдельный класс

--- src/test/java/ru/netology/pageobjects/tests/TestsLoginPage.java
+++ src/test/java/ru/netology/pageobjects/tests/TestsLoginPage.java
@@ -60,23 +60,5 @@ public class TestsLoginPage {
         val verificationCode = DataHelper.getWrongVerificationCodeFor(authInfo);
         verificationPage.canNotVerifyWrongCode(verificationCode);
     }
-
-    @Test
-    @DisplayName("Невозможно войти. Правильные логин-пароль. Превышение количества попыток введения неправильного кода")
-    void LoginFailedWrongVerificationCodeTooMuchAttemptsToPutCode() {
-        open("http://localhost:9999");
-        val loginPage = new LoginPage();
-        val authInfo = DataHelper.getCorrectAuthInfo();
-        val verificationPage = loginPage.validLogin(authInfo);
-        verificationPage.assertVerifyBtnAvailable();
-        val verificationCode1 = DataHelper.getWrongVerificationCodeFor(authInfo);
-        verificationPage.canNotVerifyWrongCode(verificationCode1);
-        val verificationCode2 = DataHelper.getWrongVerificationCodeFor(authInfo);
-        verificationPage.canNotVerifyWrongCode(verificationCode2);
-        val verificationCode3 = DataHelper.getWrongVerificationCodeFor(authInfo);
-        verificationPage.canNotVerifyWrongCode(verificationCode3);
-        val verificationCode4 = DataHelper.getWrongVerificationCodeFor(authInfo);
-        val loginPage2 = new LoginPage();
-    }
 }
 
