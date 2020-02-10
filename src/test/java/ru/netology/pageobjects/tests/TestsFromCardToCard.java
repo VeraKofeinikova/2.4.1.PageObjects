929ed9aa0fe70c862d08afa56da9c9cbfc29b7e3
Vera Kofeinikova <verak-va@mail.ru>
2020-02-10 22:59:40 +0400

    чистим куки браузеру и логинимся перед каждым тестом

--- src/test/java/ru/netology/pageobjects/tests/TestsFromCardToCard.java
+++ src/test/java/ru/netology/pageobjects/tests/TestsFromCardToCard.java
@@ -1,5 +1,6 @@
 package ru.netology.pageobjects.tests;
 
+import com.codeborne.selenide.Selenide;
 import lombok.val;
 import org.junit.jupiter.api.*;
 import ru.netology.pageobjects.data.DataHelper;
@@ -10,9 +11,11 @@ import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestsFromCardToCard {
-    @BeforeAll
-    public static void login() {
+    @BeforeEach
+    public void clearCookiesAndStorageAndLogin() {
         open("http://localhost:9999");
+        Selenide.clearBrowserCookies();
+        Selenide.clearBrowserLocalStorage();
         val loginPage = new LoginPage();
         val authInfo = DataHelper.getCorrectAuthInfo();
         val verificationPage = loginPage.happyPath(authInfo);
